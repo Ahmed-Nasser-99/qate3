83bfefbaee43c2c1ab80cde5033c52a5c94c7a0d
Ahmed-Nasser-99 <forall3020@gmail.com>
2023-10-21 14:17:55 +0300

    Introduce a logo and display it

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,13 @@
+import Header from "./components/Header";
+
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline text-white">Free Palestine</h1>
+    <div className="flex flex-col gap-3">
+      <Header />
+      <h1 className="text-3xl font-bold underline text-white">
+        Free Palestine
+      </h1>
+    </div>
   );
 }
 
---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+const Header = () => {
+  return (
+    <div className="flex items-center w-full justify-center">
+      <div className="w-[150px] h-[150px] ">
+        <img src="public/logo.png" />;
+      </div>
+    </div>
+  );
+};
+
+export default Header;

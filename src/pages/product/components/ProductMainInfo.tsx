e96685cbe5d02dc3b1aacd9ec8aa048b4efb32e0
Ahmed-Nasser-99 <forall3020@gmail.com>
2023-11-03 15:02:08 +0200

    Replace company with product

--- src/pages/product/components/ProductMainInfo.tsx
+++ src/pages/product/components/ProductMainInfo.tsx
@@ -1,9 +1,9 @@
 import ProductCard from "../../../components/ProductCard";
-import { Company } from "../../../products";
-import CompaniesList from "../../../components/ProductsList";
+import { Product } from "../../../products";
+import ProductsList from "../../../components/ProductsList";
 
 interface ProductMainInfoProps {
-  product: Company;
+  product: Product;
 }
 
 const ProductMainInfo = ({ product }: ProductMainInfoProps) => {
@@ -11,32 +11,32 @@ const ProductMainInfo = ({ product }: ProductMainInfoProps) => {
     <div className="flex flex-col  gap-4">
       <div className=" flex flex-col sm:flex-row gap-8 items-center sm:items-start">
         <div className="w-10/12 lg:w-3/12  ">
-          <ProductCard company={company} hoverToShowEffect={false} hideName />
+          <ProductCard product={product} hoverToShowEffect={false} hideName />
         </div>
         <div className="flex">
-          <CompanyInfoList company={company} />
+          <ProductInfoList product={product} />
         </div>
       </div>
       <hr className="bg-white h-[1px] w-full" />
       <div>
-        <Replacements replacements={company.replacements!} />
+        <Replacements replacements={product.replacements!} />
       </div>
     </div>
   );
 };
 
-const CompanyInfoList = ({ company }: ProductMainInfoProps) => {
+const ProductInfoList = ({ product }: ProductMainInfoProps) => {
   return (
     <div className="flex flex-col gap-5">
-      <CompanyInfoRow title="المدينة" value={company.city} />
-      <CompanyInfoRow title="المقاطعة" value={company.state} />
-      <CompanyInfoRow title="بلد المنشأ" value={company.country} />
-      <CompanyInfoRow title="الوصف" value={company.description} />
+      <ProductInfoRow title="المدينة" value={product.city} />
+      <ProductInfoRow title="المقاطعة" value={product.state} />
+      <ProductInfoRow title="بلد المنشأ" value={product.country} />
+      <ProductInfoRow title="الوصف" value={product.description} />
     </div>
   );
 };
 
-const CompanyInfoRow = ({
+const ProductInfoRow = ({
   title,
   value,
 }: {
@@ -53,12 +53,12 @@ const CompanyInfoRow = ({
   );
 };
 
-const Replacements = ({ replacements }: { replacements: Company[] }) => {
+const Replacements = ({ replacements }: { replacements: Product[] }) => {
   return (
     <div className="flex flex-col ">
       <p className="text-3xl font-bold text-green-500 mt-5">البدائل</p>
       {replacements?.length ? (
-        <CompaniesList companies={replacements} isOkList />
+        <ProductsList products={replacements} isOkList />
       ) : (
         <p>لا يوجد بدائل متاحة</p>
       )}
